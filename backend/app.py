d05294bce78c93ca7ee6a637a3788136a3938717
Karthik <jskarthik45@gmail.com>
2025-07-23 19:40:18 +0530

    v1.1

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,10 @@
-from flask import Flask, send_from_directory
-import os
+from flask import Flask
 
-app = Flask(__name__, static_folder='../frontend/dist')
+app = Flask(__name__)
 
-@app.route("/api/a")
-def a() :
-    return "a"
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve_vue_app(path):
-    static_file_path = os.path.join(app.static_folder, path)
-    if path != "" and os.path.exists(static_file_path):
-        return send_from_directory(app.static_folder, path)
-    return send_from_directory(app.static_folder, 'index.html')
+@app.route("/")
+def home() :
+    return "hello world"
 
 if __name__ == "__main__" :
     app.run()
